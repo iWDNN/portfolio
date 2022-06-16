20d401ff95b82df81309cd37a5e99a9e9610c578
iWDNN <cksgh2064@daum.net>
2022-06-16 22:53:31 +0900

    Home refactorying ing

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,97 @@
-import {
-  motion,
-  useAnimation,
-  useViewportScroll,
-  Variants,
-} from "framer-motion";
-import { useEffect } from "react";
 import styled from "styled-components";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Home from "./Components/Home";
-import Work from "./Components/Work";
-
-const Background = styled(motion.div)`
-  background-color: transparent;
+interface IText {
+  size?: string;
+  color?: string;
+  weight?: number;
+  space?: string;
+  position?: string;
+  top?: string;
+  bottom?: string;
+}
+interface ITextBox {
+  top?: string;
+  bottom?: string;
+}
+const Background = styled.div`
+  background-color: yellowgreen;
 `;
-
-const bodyAni: Variants = {
-  top: {
-    backgroundColor: "rgba(0,0,0,1)",
-  },
-  scroll: {
-    backgroundColor: "rgba(0,0,0,0)",
-  },
-};
-
+const Section = styled.section<{ height?: string; bgColor: string }>`
+  position: relative;
+  background: ${(props) => props.bgColor};
+  width: 100%;
+  height: ${(props) => props.height};
+  text-align: center;
+`;
+const TextBox = styled.div<ITextBox>`
+  position: absolute;
+  top: ${(props) => props.top};
+  bottom: ${(props) => props.bottom};
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
+const Text = styled.p<IText>`
+  padding: 8px;
+  text-align: center;
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.size};
+  font-weight: ${(props) => props.weight};
+  letter-spacing: ${(props) => props.space};
+  position: ${(props) => props.position};
+  top: ${(props) => props.top};
+  bottom: ${(props) => props.bottom};
+  span {
+    width: 600px;
+    line-height: 1.4;
+  }
+`;
+const HeadingText = styled.h1`
+  position: absolute;
+  bottom: 300px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  font-weight: 700;
+  font-size: 25vw;
+  letter-spacing: 10px;
+  color: #3f72af;
+`;
 function App() {
-  const { scrollY } = useViewportScroll();
-  const background = useAnimation();
-  useEffect(() => {
-    scrollY.onChange(() => {
-      if (scrollY.get() >= 200) {
-        background.start("top");
-      } else {
-        background.start("scroll");
-      }
-    });
-  }, [scrollY]);
   return (
     <>
-      <Background
-        variants={bodyAni}
-        animate={background}
-        transition={{ type: "tween" }}
-      >
+      <Background>
         <Header />
-        <Home />
-        <About />
-        <Work />
-        <Contact />
+        <Section bgColor={"pink"} height={"120vh"}>
+          <TextBox top={"50px"}>
+            <Text space={"1px"} size={"15px"} color={"#5c5b5c"} weight={500}>
+              COMING SOON
+            </Text>
+            <Text size={"50px"} weight={700}>
+              New Year's Revolution.
+            </Text>
+          </TextBox>
+          <HeadingText>Pocket</HeadingText>
+          <TextBox bottom={"0px"}>
+            <Text size={"15px"} color={"#b21a59"} weight={800} space="1px">
+              NEW PROJECT POCKET
+            </Text>
+            <Text size={"50px"} color={"#fff"} weight={700}>
+              Everything, but small.
+            </Text>
+            <Text size={"17px"} color={"#949394"} weight={500}>
+              <span>
+                It's Procreate Pocket, but not as you know it. Be the first to
+                know more, <br />
+                sign up and find out soon.
+              </span>
+            </Text>
+          </TextBox>
+        </Section>
+        <Section bgColor="beige" height="100vh">
+          <Text>Skills</Text>
+        </Section>
         <Footer />
       </Background>
     </>
--- src/Components/Work.tsx
+++ src/Components/Work.tsx
@@ -1,4 +1,4 @@
-function Work(){
-  return <h1>Work</h1>
+function Work() {
+  return <h1>Work</h1>;
 }
 export default Work;
